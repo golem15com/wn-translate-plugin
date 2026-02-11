3135eeadecb04742dd853c83026abafe566a7e0e
Jakub Zych <jakub@keios.eu>
2026-02-11 15:09:22 +0100

    Fix cache clearing

--- classes/TranslatableBehavior.php
+++ classes/TranslatableBehavior.php
@@ -317,6 +317,21 @@ abstract class TranslatableBehavior extends ExtensionBase
         }
 
         /*
+         * Invalidate translation cache for all locales when saving
+         */
+        if ($this->model->exists) {
+            $modelType = $this->model->getMorphClass();
+            $modelId = $this->model->getKey();
+
+            $locales = array_keys(\Golem15\Translate\Models\Locale::listEnabled());
+
+            foreach ($locales as $locale) {
+                $cacheKey = sprintf('translation:%s:%s:%s', $modelType, $modelId, $locale);
+                \Cache::forget($cacheKey);
+            }
+        }
+
+        /*
          * Saving the default locale, no need to restore anything
          */
         if ($this->translatableContext == $this->translatableDefault) {
@@ -331,22 +346,6 @@ abstract class TranslatableBehavior extends ExtensionBase
         $translatable = $this->model->getTranslatableAttributes();
         $originalValues = array_intersect_key($original, array_flip($translatable));
         $this->model->attributes = array_merge($attributes, $originalValues);
-
-        /*
-         * Invalidate Redis translation cache for all locales when saving
-         */
-        if ($this->model->exists) {
-            $modelType = $this->model->getMorphClass();
-            $modelId = $this->model->getKey();
-
-            // Get all enabled locales from Locale model
-            $locales = array_keys(\Golem15\Translate\Models\Locale::listEnabled());
-
-            foreach ($locales as $locale) {
-                $cacheKey = sprintf('translation:%s:%s:%s', $modelType, $modelId, $locale);
-                \Cache::forget($cacheKey);
-            }
-        }
     }
 
     /**
